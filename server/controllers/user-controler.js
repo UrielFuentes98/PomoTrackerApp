e2a3704c82cab9fe160fbee36f2b2d9a9af62871
Uriel Fuentes <urielfuentesc@gmail.com>
2021-02-18 19:07:54 -0600

    Add log messages to backend.

--- server/controllers/user-controler.js
+++ server/controllers/user-controler.js
@@ -10,7 +10,7 @@ const { User, Time } = require("../models");
 ========================================================= */
 router.post("/register", async (req, res) => {
   const salt = bcrypt.genSaltSync(saltRounds);
-  const hash = bcrypt.hashSync(req.body.password, salt); 
+  const hash = bcrypt.hashSync(req.body.password, salt);
   try {
     // create a new user with the password hash from bcrypt
     let user = await User.create(Object.assign(req.body, { password: hash }));
@@ -18,13 +18,16 @@ router.post("/register", async (req, res) => {
     // data will be an object with the user and it's authToken
     let tokenObj = await user.authorize();
 
+    console.log("POST: /register. User registered.");
     // send back the new user and auth token to the client
     return res
       .status(200)
       .cookie("auth_token", tokenObj.token)
-      .send("User registered."); 
-  } catch (err) {
-    return res.status(400).send(err.message);
+      .send("User registered.");
+  } catch (error) {
+    const errors = error.errors.map((err) => err.message);
+    console.error("Register validation error: ", errors);
+    return res.status(400).json(errors);
   }
 });
 
@@ -40,8 +43,10 @@ router.post("/login", async (req, res) => {
   try {
     const tokenObj = await User.authenticate(user_id, password);
 
+    console.log("POST: /login. User logged in.");
     return res.cookie("auth_token", tokenObj.token).send("User logged in");
   } catch (err) {
+    console.error("Error. POST: /login.", err.message);
     return res.status(400).send(err.message);
   }
 });
@@ -56,12 +61,17 @@ router.delete("/logout", async (req, res) => {
   if (auth_token) {
     try {
       await User.logout(auth_token);
+
+      console.log("DELETE: /logout. Session finished.");
+      res.clearCookie("auth_token");
       return res.status(200).send("Session finished.");
     } catch (err) {
+      console.error("Error. DELETE: /logout.", err.message);
       return res.status(400).send(err.message);
     }
   }
 
+  console.log("Error. DELETE: /logout. No session token provided.");
   //Session not provided. Bad request.
   return res.status(400).send("No session token provided.");
 });
@@ -73,20 +83,22 @@ router.post("/sendRecord", async (req, res) => {
   const { user } = req;
   Time.updateRecord(user, time, pomodoro)
     .then((result) => {
+      console.log("POST: /sendRecord. OK 200");
       res.status(200).send(result);
     })
     .catch((error) => {
-      console.log(error);
-      res.status(400).send(error);
+      console.error("Error. POST /sendRecord.", error.message);
+      res.status(400).send(error.message);
     });
 });
 
 router.get("/main-stats", async (req, res) => {
   try {
     let stats = await Time.getStats(req.user);
+    console.log("GET: /main-stats. OK 200");
     res.status(200).json(stats);
   } catch (error) {
-    console.log(error);
+    console.error("Error. GET /main-stats.", error.message);
     res.status(400);
     res.send(error.message);
   }
